e8705f7ddb7f98e235d6ee9ae8aa683401c863e1
fardellatimothe <timothe.fardella@viacesi.fr>
2025-03-26 16:19:58 +0100

    Front + Docker

--- index.php
+++ index.php
@@ -1,53 +1,14 @@
 <?php
-/**
- * This is the router, the main entry point of the application.
- * It handles the routing and dispatches requests to the appropriate controller methods.
- */
 
-require "vendor/autoload.php";
+// Inclut l'autoloader de Composer
+require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Controllers\TaskController;
+// Charge Twig
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader);
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig = new \Twig\Environment($loader, [
-    'debug' => true
-]);
+// Charge le template "home.html.twig"
+$template = $twig->load('home.html.twig');
 
-if (isset($_GET['uri'])) {
-    $uri = $_GET['uri'];
-} else {
-    $uri = '/';
-}
-
-$controller = new TaskController($twig);
-
-switch ($uri) {
-    case '/':
-        // TODO : call the welcomePage method of the controller
-        echo 'Welcome page';
-        break;
-    case 'add_task':
-        // TODO : call the addTask method of the controller
-        echo 'Add task action';
-        break;
-    case 'check_task':
-        // TODO : call the checkTask method of the controller
-        echo 'Check task action';
-        break;
-    case 'history':
-        // TODO : call the historyPage method of the controller
-        echo 'History page';
-        break;
-    case 'uncheck_task':
-        // TODO : call the uncheckTask method of the controller
-        echo 'Uncheck task action';
-        break;
-    case 'about':
-        // TODO : call the aboutPage method of the controller
-        echo 'About page';
-        break;
-    default:
-        // TODO : return a 404 error
-        echo '404 Not Found';
-        break;
-}
+// Rendre le template avec des données (si nécessaire)
+echo $template->render();
