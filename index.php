593ce6a6f0dd69774964c9ba77a1b11bd97243fe
fardellatimothe <timothe.fardella@viacesi.fr>
2025-03-28 16:11:13 +0100

    Update BDD

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Controllers\DirController;
 
-$host = 'db'; 
+$host = '20.126.97.69'; 
 $dbname = 'stageconnectbdd'; 
 $username = 'root'; 
 $password = 'root123'; 
