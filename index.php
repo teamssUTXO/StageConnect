5a38d68da0e27cd7647eb8ef69b1737bc297af90
antoineP-it <antoine.pouzoulet@viacesi.fr>
2025-03-28 11:18:50 +0100

    Creation du fichier de connexion et ajout des methodes CRUD

--- index.php
+++ index.php
@@ -3,17 +3,6 @@
 // Inclut l'autoloader de Composer
 require_once __DIR__ . '/vendor/autoload.php';
 
-$host = 'db'; 
-$dbname = 'stageconnectbdd'; 
-$username = 'root'; 
-$password = 'root123'; 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-} catch (PDOException $e ){
-    echo 'Erreur de connexion à la base de données : ' . $e->getMessage();
-}
 
 // Charge Twig
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
