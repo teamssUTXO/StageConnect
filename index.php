f2d157db1efcfba1957048c783c737ce767532c0
fardellatimothe <timothe.fardella@viacesi.fr>
2025-04-04 10:38:51 +0200

    Fix bugs

--- index.php
+++ index.php
@@ -156,6 +156,13 @@ switch ($segments[0]) {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
+
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+
         if (isset($_SESSION['user'])) {
             if (count($segments) === 3 && $segments[0] === 'wishlist' && $segments[1] === 'toggle' && is_numeric($segments[2])) {
                 $offerId = $segments[2];
@@ -262,124 +269,173 @@ switch ($segments[0]) {
         }
         break;
 
-        case 'updateUser':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controlleruser->updateUser();
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
+    case 'updateUser':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controlleruser->updateUser();
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
 
-        case 'createUser':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controlleruser->createUser(); // Appelle la méthode createUser du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
+    case 'createUser':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controlleruser->createUser(); // Appelle la méthode createUser du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
 
-        case 'deleteUser':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-                $controlleruser->deleteUser(); // Appelle la méthode deleteUser du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-                http_response_code(405); // Méthode non autorisée
-            }
-            break;
+    case 'deleteUser':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $controlleruser->deleteUser(); // Appelle la méthode deleteUser du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+            http_response_code(405); // Méthode non autorisée
+        }
+        break;
 
-        case 'rating':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if (isset($_SESSION['user'])) {
-                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($segments[1])) {
-                    $siret = $segments[1];
-                
-                    $controllercompany->rating($siret);
-                    exit();
-                } else {
-                    echo '404 Not Found';
-                }
+    case 'rating':
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+        }
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (isset($_SESSION['user'])) {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($segments[1])) {
+                $siret = $segments[1];
+            
+                $controllercompany->rating($siret);
+                exit();
             } else {
-                $controller->loginPage();
+                echo '404 Not Found';
             }
-            break;
+        } else {
+            $controller->loginPage();
+        }
+        break;
 
-        case 'updateCompany':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controllercompany->updateCompany();
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
+    case 'updateCompany':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controllercompany->updateCompany();
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
 
 
-        case 'createCompany':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controllercompany->createCompany(); // Appelle la méthode createCompany du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
+    case 'createCompany':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controllercompany->createCompany(); // Appelle la méthode createCompany du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
 
-        case 'deleteCompany':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-                $controllercompany->deleteCompany(); // Appelle la méthode deleteCompany du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-                http_response_code(405); // Méthode non autorisée
-            }
-            break;
-        
-        case 'updateOffer' : 
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controlleroffer->updateOffer();
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
-        case 'createOffer' : 
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $controlleroffer->createOffer(); // Appelle la méthode createCompany du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-            }
-            break;
-        case 'deleteOffer':
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-                $controlleroffer->deleteOffer(); // Appelle la méthode deleteCompany du contrôleur
-            } else {
-                echo 'Méthode non autorisée.';
-                http_response_code(405); // Méthode non autorisée
-            }
-            break;
+    case 'deleteCompany':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $controllercompany->deleteCompany(); // Appelle la méthode deleteCompany du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+            http_response_code(405); // Méthode non autorisée
+        }
+        break;
+    
+    case 'updateOffer' : 
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controlleroffer->updateOffer();
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
+    case 'createOffer' : 
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controlleroffer->createOffer(); // Appelle la méthode createCompany du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+        }
+        break;
+    case 'deleteOffer':
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $user = $_SESSION['user'];
+        if ($user->Id_Role == 2 || $user->Id_Role == 1) {
+            $_SESSION['flash'] = "Vous ne pouvez pas ajouter une offre dans votre wishlist.";
+            die;
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $controlleroffer->deleteOffer(); // Appelle la méthode deleteCompany du contrôleur
+        } else {
+            echo 'Méthode non autorisée.';
+            http_response_code(405); // Méthode non autorisée
+        }
+        break;
 
     default:
         echo '404 Not Found';
